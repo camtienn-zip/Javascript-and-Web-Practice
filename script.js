33dd88c2c10cebaac2c4a7df108f034e084fb887
Camtien Nguyen <camtienn153@gmail.com>
2023-10-16 19:50:10 -0400

    added script.js and index.html and expected_result

---
+++ script.js
@@ -0,0 +1,82 @@
+let allMovies = [];
+
+//Define a movie class with parameters title (string), rating (number) and haveWatched (boolean)
+class Movie {
+    constructor(title, rating, haveWatched) {
+        this.title = title;
+        this.rating = rating;
+        this.haveWatched = haveWatched;
+    }
+}
+
+//add a movie OBJECT to the allMovies array
+let addMovie = (movie) => {
+    allMovies.push(movie);
+}
+
+//iterate through all elements of allMovies array
+//Display the total number of movies in allMovies array
+let printMovies = () => {
+    console.log(`You have ${allMovies.length} movies in total`);
+    allMovies.forEach(movie => {
+        console.log(`${movie.title}, rating of ${movie.rating}, haveWatched: ${movie.haveWatched}`);
+    });
+}
+
+
+//Display only the movies that has a rating higher than rating(argument)
+//Display the total number of matches
+let highRatings = (rating) => {
+
+}
+
+
+//Toggle the 'haveWatched' property of the specified movie 
+let changeWatched = (title) => {
+
+}
+
+
+
+////////////////////////////////////////////////////////////
+//Test code - DO NOT DELETE
+let x = new Movie("Spiderman", 3, true);
+let y = new Movie("Citizen Kane", 4, false);
+let z = new Movie("Zootopia", 4.5, true);
+
+allMovies.push(x,y,z);
+
+/*replace console.log with display on web page*/
+console.log("----------------");
+console.log("running program......");
+console.log("----------------");
+printMovies();
+
+
+let movie1 = new Movie("Parasite", 2, false);
+
+/*replace console.log with display on web page*/
+console.log("----------------");
+addMovie(movie1);
+console.log("----------------");
+
+
+
+changeWatched("Spiderman");
+/*replace console.log with display on web page*/
+console.log("----------------");
+
+printMovies();
+
+/*replace console.log with display on web page*/
+console.log("----------------");
+
+changeWatched("Spiderman");
+/*replace console.log with display on web page*/
+console.log("----------------");
+
+printMovies();
+/*replace console.log with display on web page*/
+console.log("----------------");
+
+highRatings(3.5);
