1c585c2dd7fbce2b578f47fe460c14f0877861b8
Camtien Nguyen <camtienn153@gmail.com>
2023-10-16 22:12:38 -0400

    completed assignment

--- script.js
+++ script.js
@@ -1,40 +1,61 @@
 let allMovies = [];
 
-//Define a movie class with parameters title (string), rating (number) and haveWatched (boolean)
+// Define a movie class with parameters title (string), rating (number), and haveWatched (boolean)
 class Movie {
-    constructor(title, rating, haveWatched) {
+    constructor(title, rating, havewatched) {
         this.title = title;
         this.rating = rating;
-        this.haveWatched = haveWatched;
+        this.havewatched = havewatched;
+    }
+}
+let result = ''; // Initialize the result variable to accumulate output
+
+// Add a movie OBJECT to the allMovies array
+let addMovie = (movie) => {
+    allMovies.push(movie);
+    result += "<h2>A new movie is added</h2>";
+    printMovies(); // Call the printMovies function to update the result
+}
+
+
+// Iterate through all elements of allMovies array
+// Display the total number of movies in allMovies array
+let printMovies = () => {
+    result += "<h2>Printing all movies...</h2>";
+    allMovies.forEach(movie => {
+        result += `<p>${movie.title}, rating of ${movie.rating}, havewatched: ${movie.havewatched}</p>`;
+    });
+    result += `<p>You have ${allMovies.length} movies in total</p>`;
+    updateResult(); // Call the updateResult function to display the result
+}
+
+// Display only the movies that have a rating higher than rating(argument)
+// Display the total number of matches
+let highRatings = (rating) => {
+    let highRatedMovies = allMovies.filter(movie => movie.rating > rating);
+    result += `<h2>Movies with a rating higher than ${rating}</h2>`;
+    highRatedMovies.forEach(movie => {
+        result += `<p>${movie.title} has a rating of ${movie.rating}</p>`;
+    });
+    result += `<p>In total, there are ${highRatedMovies.length} matches</p>`;
+    updateResult(); // Call the updateResult function to display the result
+}
+
+// Toggle the 'haveWatched' property of the specified movie
+let changeWatched = (title) => {
+    const movie = allMovies.find(movie => movie.title === title);
+    if (movie) {
+        movie.havewatched = !movie.havewatched;
+        result += "<h2>Changing the status of the movie...</h2>";
+        updateResult(); // Call the updateResult function to display the result
     }
 }
 
-//add a movie OBJECT to the allMovies array
-let addMovie = (movie) => {
-    allMovies.push(movie);
+// Function to update the result displayed on the web page
+let updateResult = () => {
+    document.getElementById("result").innerHTML = result;
 }
 
-//iterate through all elements of allMovies array
-//Display the total number of movies in allMovies array
-let printMovies = () => {
-    console.log(`You have ${allMovies.length} movies in total`);
-    allMovies.forEach(movie => {
-        console.log(`${movie.title}, rating of ${movie.rating}, haveWatched: ${movie.haveWatched}`);
-    });
-}
-
-
-//Display only the movies that has a rating higher than rating(argument)
-//Display the total number of matches
-let highRatings = (rating) => {
-
-}
-
-
-//Toggle the 'haveWatched' property of the specified movie 
-let changeWatched = (title) => {
-
-}
 
 
 
